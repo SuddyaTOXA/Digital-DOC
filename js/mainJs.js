a51fdabcc05bea471014d20ac4d19c0d89042824
Anton Shlikhta <suddyadred@gmail.com>
2016-07-30 20:46:46 +0300

    Suddya

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
+    // for Owl Carousel sliders
+    $('.main-item-slider').owlCarousel({
+        navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
 
+        // "singleItem:true" is a shortcut for:
+        // items : 1,
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+    });
 });
