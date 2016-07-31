f329ae829b82510f6f22eee90e3e775de807e2f6
Anton Shlikhta <suddyadred@gmail.com>
2016-07-31 16:25:19 +0300

    Suddya

--- js/mainJs.js
+++ js/mainJs.js
@@ -5,12 +5,21 @@ $(document).ready(function() {
         margin:10,
         nav:true,
         items:1
-
-        // "singleItem:true" is a shortcut for:
-        // items : 1,
-        // itemsDesktop : false,
-        // itemsDesktopSmall : false,
-        // itemsTablet: false,
-        // itemsMobile : false
     });
+    //cache a reference to the tabsd
+    var tabs = $('.tabs li');
+
+    //on click to tab, turn it on, and turn previously-on tab off
+    tabs.click(function() { $(this).addClass('on').siblings('.on').removeClass('on'); });
+
+    //auto-rotate every 5 seconds
+    setInterval(function() {
+
+        //get currently-on tab
+        var onTab = tabs.filter('.on');
+
+        //click either next tab, if exists, else first one
+        var nextTab = onTab.index() < tabs.length-1 ? onTab.next() : tabs.first();
+        nextTab.click();
+    }, 5000);
 });
