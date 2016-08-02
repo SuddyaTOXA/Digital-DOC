01cf9aa2ba6b75a3eb90afeeb4ed256ebc25fab1
Anton Shlikhta <suddyadred@gmail.com>
2016-08-03 01:18:24 +0300

    Suddya

--- js/mainJs.js
+++ js/mainJs.js
@@ -28,31 +28,38 @@ $(document).ready(function() {
         items:1
     });
     //cache a reference to the tabsd
-    var tabs = $('.tabs li');
-    var tabsListBox = $('.box-tabs-list > li');
-    //on click to tab, turn it on, and turn previously-on tab off
-    tabs.click(function() {
-        var liId = $(this).index();
-        $(this).addClass('on').siblings('.on').removeClass('on');
-        tabsListBox.eq(liId).addClass('visible').siblings('.visible').removeClass('visible');
+
+
+    $(window).on('load resize', function() {
+        var tabs = $('.tabs li'),
+            tabsListBox = $('.box-tabs-list > li'),
+            tabsMini = $('.tabs-mini');
+
+        if ($(window).width() <= '640') {
+            tabsMini.click(function() {
+                $(this).parent().addClass('visible').siblings('.visible').removeClass('visible');
+            });
+
+
+        } if ($(window).width() > '640') {
+            tabs.click(function() {
+                var liId = $(this).index();
+                $(this).addClass('on').siblings('.on').removeClass('on');
+                tabsListBox.eq(liId).addClass('visible').siblings('.visible').removeClass('visible');
+            });
+
+            //setInterval(function() {
+            //
+            //    //get currently-on tab
+            //    var onTab = tabs.filter('.on');
+            //    var onBox = tabsListBox.filter('.visible');
+            //    //click either next tab, if exists, else first one
+            //    var nextTab = onTab.index() < tabs.length-1 ? onTab.next() : tabs.first();
+            //    nextTab.click();
+            //    var nextBox = onBox.index() < tabsListBox.length-1 ? onBox.next() : tabsListBox.first();
+            //    nextBox.click();
+            //}, 7000);
+        }
     });
 
-
-    var offSetTop = $("#person-slider").offset().top,
-        elementScroll = $("#person-slider");
-
-
-    //auto-rotate every 5 seconds
-    //setInterval(function() {
-    //
-    //    //get currently-on tab
-    //    var onTab = tabs.filter('.on');
-    //    var onBox = tabsListBox.filter('.visible');
-    //    //click either next tab, if exists, else first one
-    //    var nextTab = onTab.index() < tabs.length-1 ? onTab.next() : tabs.first();
-    //    nextTab.click();
-    //    var nextBox = onBox.index() < tabsListBox.length-1 ? onBox.next() : tabsListBox.first();
-    //    nextBox.click();
-    //}, 7000);
-    //
 });
