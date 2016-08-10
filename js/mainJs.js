e963632c21a57af636a5d9ac3fece5b14330a170
Anton Shlikhta <suddyadred@gmail.com>
2016-08-10 14:27:34 +0300

    Suddya

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,24 +1,11 @@
 $(document).ready(function() {
 
-    // scroll navigation fixed
-    //if ($(window).width() > '974') {
-    //    $(window).scroll(function(){
-    //        $('.header').toggleClass('scroll-js', $(this).scrollTop() > 0);
-    //    });
-    //}
-
     // for burger menu
     $('.mobile-menu-toggle, .mobile-menu-overlay').on('click', function(){
         $('.mobile-menu-toggle').toggleClass('active');
         $('.mobile-menu-wrap').toggleClass('showing');
         $(document.body).toggleClass('overflow');
     });
-    //$(".mobile-menu a").on('click', function(){
-    //    $(this).toggleClass('open').siblings('.sub-menu').slideToggle(350);
-    //});
-    //$(window).on('load', function(){
-    //    $(".mobile-menu a + .sub-menu").siblings('a').toggleClass('expanded').attr("href", "#");
-    //});
 
     // for Owl Carousel sliders
     $('.main-item-slider').owlCarousel({
@@ -41,6 +28,7 @@ $(document).ready(function() {
             tabsLi.removeClass('on');
             $(".box").attr('style', '');
 
+            tabsMini.unbind('click');
             tabsMini.click(function() {
                 $(this).next().toggle('500');
             });
